5f6e30541407965131e29b5101bac0d791bdf6b4
taniaa-sh <taniashafiee78@gmail.com>
2025-04-18 04:41:23 -0700

    navbar for mobile

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -1,30 +1,48 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import MenuOverlay from "./MenuOverlay";
+
+const navLinkItems = [
+  {
+      href: "/",
+      title: "about",
+  },
+  {
+      href: "/projects",
+      title: "projects",
+  },
+  {
+      href: "/contact",
+      title: "contact",
+  },
+];
 
 function Navbar() {
 
-    const navLinkItems = [
-        {
-            href: "/",
-            title: "about",
-        },
-        {
-            href: "/projects",
-            title: "projects",
-        },
-        {
-            href: "/contact",
-            title: "contact",
-        },
-    ];
+    const [navbarOpen,setNavbarOpen] = useState(false)
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-gray-500/20 backdrop-blur-md shadow-sm border-b border-pink-300">
       <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link href="/">
           <span className="text-xl md:text-5xl font-semibold text-white">Portfolio</span>
         </Link>
+        <div className="block md:hidden mobile-menu">
+         {
+          !navbarOpen ? (
+            <button onClick={() => setNavbarOpen(true)} className="text-slate-200 flex items-center px-3 py-2 border border-slate-200 hover:text-white hover:border-white">
+               <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button onClick={() => setNavbarOpen(false)} className="text-slate-200 flex items-center px-3 py-2 border border-slate-200 hover:text-white hover:border-white">
+               <XMarkIcon className="h-5 w-5" />
+            </button>
+          )
+         }
+        </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
             <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
                 {
@@ -37,6 +55,9 @@ function Navbar() {
             </ul>
         </div>
       </div>
+      {
+        navbarOpen ? <MenuOverlay items={navLinkItems} /> : null
+      }
     </nav>
   );
 }
