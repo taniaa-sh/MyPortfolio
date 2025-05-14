2279dec774208bad1297f2c409e8e767040f9f8d
tania shafiee <taniashafiee78@gmail.com>
2025-05-14 13:42:17 +0330

    fix hire me modal

--- portfolio/components/HireMeModal.jsx
+++ portfolio/components/HireMeModal.jsx
@@ -1,12 +1,13 @@
 "use client";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const HireMeModal = (props) => {
   const [isClosing, setIsClosing] = useState(false);
 
   const handleClose = () => {
-    setIsClosing(true);    
+    setIsClosing(true);
     props.setShowModal(false);
     setIsClosing(false);
   };
@@ -29,11 +30,30 @@ const HireMeModal = (props) => {
       className="fixed inset-0 bg-white/40 flex justify-center items-center z-10"
       onClick={handleBackdropClick}
     >
-      <div className="bg-gradient-to-br z-50 from-purple-600 via-purple-800 to-black text-white p-6 rounded-lg max-w-sm w-full">
+      <div className="bg-gradient-to-br z-50 from-purple-600 via-purple-800 to-black text-white p-6 rounded-lg max-w-sm w-full flex flex-col gap-4">
         <h2 className="text-2xl !mb-4">Contact Information</h2>
-        <p>Email: taniashafiee78@gmail.com</p>
-        <p>Phone: 09332080083</p>
-        <p>LinkedIn: linkedin.com/in/tania-shafiee</p>
+        <Link
+          href="mailto:taniashafiee78@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Email: taniashafiee78@gmail.com
+        </Link>
+
+        <Link
+          target="_blank"
+          href={"tel:100063"}
+          onClick={() => window.open("tel:100063")}
+        >
+          Phone: 09332080083
+        </Link>
+        <Link
+          target="_blank"
+          href={"https://linkedin.com/in/tania-shafiee"}
+          onClick={() => window.open("https://linkedin.com/in/tania-shafiee")}
+        >
+          LinkedIn: linkedin.com/in/tania-shafiee
+        </Link>
         <button
           className="cursor-pointer !mt-4 w-full sm:w-fit px-4 py-2 rounded-full bg-purple-500 hover:bg-purple-600 text-white"
           onClick={handleClose}
