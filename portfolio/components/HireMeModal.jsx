3d4effd4e742817a2a735211ae6991d68d91845e
tania shafiee <taniashafiee78@gmail.com>
2025-05-14 09:15:03 +0330

    fix hire me modal bugs and add some feacher to it

--- portfolio/components/HireMeModal.jsx
+++ portfolio/components/HireMeModal.jsx
@@ -1,42 +1,48 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 
-
 const HireMeModal = (props) => {
-    const [isClosing, setIsClosing] = useState(false);
-    const [showLoading, setShowLoading] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
 
-    const handleClose = () => {
-        setIsClosing(true);
-        setTimeout(() => {
-            props.setShowModal(false)
-            setIsClosing(false)
-        }, 250);
+  const handleClose = () => {
+    setIsClosing(true);    
+    props.setShowModal(false);
+    setIsClosing(false);
+  };
+
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) {
+      handleClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+    return () => {
+      document.body.style.overflowY = "auto";
     };
+  }, []);
 
-    useEffect(() => {
-        document.body.style.overflowY = 'hidden'
-        return () => {
-            document.body.style.overflowY = 'auto'
-        }
-    }, [])
-
-    return (
-        <div className="fixed inset-0 bg-white/40 flex justify-center items-center z-10">
-        <div className="bg-gradient-to-br z-50 from-purple-600 via-purple-800 to-black text-white p-6 rounded-lg max-w-sm w-full">
-          <h2 className="text-2xl !mb-4">Contact Information</h2>
-          <p>Email: taniashafiee78@gmail.com</p>
-          <p>Phone: 09332080083</p>
-          <p>LinkedIn: linkedin.com/in/tania-shafiee</p>
-          <button
-            className="cursor-pointer !mt-4 w-full sm:w-fit px-4 py-2 rounded-full bg-purple-500 hover:bg-purple-600 text-white"
-            onClick={handleCloseModal}
-          >
-            Close
-          </button>
-        </div>
+  return (
+    <div
+      className="fixed inset-0 bg-white/40 flex justify-center items-center z-10"
+      onClick={handleBackdropClick}
+    >
+      <div className="bg-gradient-to-br z-50 from-purple-600 via-purple-800 to-black text-white p-6 rounded-lg max-w-sm w-full">
+        <h2 className="text-2xl !mb-4">Contact Information</h2>
+        <p>Email: taniashafiee78@gmail.com</p>
+        <p>Phone: 09332080083</p>
+        <p>LinkedIn: linkedin.com/in/tania-shafiee</p>
+        <button
+          className="cursor-pointer !mt-4 w-full sm:w-fit px-4 py-2 rounded-full bg-purple-500 hover:bg-purple-600 text-white"
+          onClick={handleClose}
+        >
+          Close
+        </button>
       </div>
-    );
-}
-export default HireMeModal
+    </div>
+  );
+};
+
+export default HireMeModal;
