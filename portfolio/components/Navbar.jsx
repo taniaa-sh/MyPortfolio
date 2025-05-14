b84ecce2769bbd72e2a3b3585684ba7ed0406ff9
tania shafiee <taniashafiee78@gmail.com>
2025-05-14 09:15:44 +0330

    edit nabar component

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -2,68 +2,71 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./NavLink";
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 
 const navLinkItems = [
   {
-      href: "#about",
-      title: "about",
+    href: "#about",
+    title: "about",
   },
   {
-      href: "#projects",
-      title: "projects",
+    href: "#projects",
+    title: "projects",
   },
   {
-      href: "#contact",
-      title: "contact",
+    href: "#contact",
+    title: "contact",
   },
 ];
 
 function Navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
-    const [navbarOpen,setNavbarOpen] = useState(false)
-
-    // fixed top-4 left-8 right-8 z-50 flex-row items-center justify-center gap-8 lg:gap-10 xl:gap-12 select-none backdrop-filter py-[1px] px-4 bg-brown-900/50 rounded-3xl backdrop-blur-[5px]
   return (
-    <nav 
+    <nav
       className="fixed top-0 left-0 right-0 z-50 bg-gray-500/50 backdrop-blur-[5px] shadow-sm border-b border-pink-300"
       style={{
-        backdropFilter: "blur(5px)"
+        backdropFilter: "blur(5px)",
       }}
     >
       <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link href="/">
-          <span className="text-xl md:text-5xl font-semibold text-white">Portfolio</span>
+          <span className="text-xl md:text-5xl font-semibold text-white">
+            Portfolio
+          </span>
         </Link>
         <div className="block md:hidden mobile-menu">
-         {
-          !navbarOpen ? (
-            <button onClick={() => setNavbarOpen(true)} className="text-slate-200 flex items-center px-3 py-2 border border-slate-200 hover:text-white hover:border-white">
-               <Bars3Icon className="h-5 w-5" />
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="text-slate-200 flex items-center px-3 py-2 border border-slate-200 hover:text-white hover:border-white cursor-pointer"
+            >
+              <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
-            <button onClick={() => setNavbarOpen(false)} className="text-slate-200 flex items-center px-3 py-2 border border-slate-200 hover:text-white hover:border-white">
-               <XMarkIcon className="h-5 w-5" />
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="text-slate-200 flex items-center px-3 py-2 border border-slate-200 hover:text-white hover:border-white"
+            >
+              <XMarkIcon className="h-5 w-5" />
             </button>
-          )
-         }
+          )}
         </div>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-            <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 !mt-0">
-                {
-                    navLinkItems.map((item, index) => (
-                        <li className="inline-block" key={index}>
-                            <NavLink href={item.href} title={item.title} />
-                        </li>
-                    ))
-                }
-            </ul>
+        <div
+          className="menu hidden md:block md:w-auto mx-auto pr-60"
+          id="navbar"
+        >
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 !mt-0">
+            {navLinkItems.map((item, index) => (
+              <li className="inline-block" key={index}>
+                <NavLink href={item.href} title={item.title} />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-      {
-        navbarOpen ? <MenuOverlay items={navLinkItems} /> : null
-      }
+      {navbarOpen ? <MenuOverlay items={navLinkItems} /> : null}
     </nav>
   );
 }
