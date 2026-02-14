4a38f5f8ec2494abaa1dfa04b0723620e209acb4
tania shafiee <taniashafiee78@gmail.com>
2026-02-14 11:37:18 +0330

    fix ui bug

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-gradient-to-tr from-purple-900 via-black to-purple-800 text-white py-8 mt-24">
-      <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
-        <div className="text-center md:text-left">
+    <footer className="w-full bg-gradient-to-tr from-purple-900 via-black to-purple-800 text-white py-8">
+      <div className="container mx-auto px-4 flex flex-col items-center justify-between">
+        <div className="text-center">
           {/* <h2 className="text-xl font-semibold">طراحی‌شده با ❤️ توسط [تانیا]</h2> */}
           {/* <p className="text-sm text-gray-300 !mt-2">
             © {new Date().getFullYear()} همه‌ی حقوق محفوظ است.
           </p> */}
         </div>
-        <div className="flex gap-4 md:!mt-0">
+        <div className="flex gap-4">
           <Link
             href="https://github.com/taniaa-sh"
             target="_blank"
